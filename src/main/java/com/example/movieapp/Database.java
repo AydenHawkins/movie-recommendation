cae40f3a556486191318ba053e9304b3f43efbea
ch7025 <cjh7025@uncw.edu>
2024-11-21 16:11:45 -0500

    started work on insert_liked_movies

--- src/main/java/com/example/movieapp/Database.java
+++ src/main/java/com/example/movieapp/Database.java
@@ -1,4 +1,18 @@
 package com.example.movieapp;
 
 public class Database {
+
+    public static void insert_liked_movie(int id){
+        String url = "jdbc:sqlite:movieuserdata.db";
+
+        String sql = String.format("INSERT INTO Liked_Movies VALUES(%d);", id);
+    }
+
+    public static void insert_watched_movie(){
+
+    }
+
+    public static void insert_watch_list(){
+
+    }
 }
