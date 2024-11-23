98903102d5e830dc07e53e76af0f4b82e4190abe
AydenHawkins <aydenhawkins17@gmail.com>
2024-11-23 17:45:44 -0500

    Fixed display bug when changing scenes

--- src/main/java/com/example/movieapp/SceneManager.java
+++ src/main/java/com/example/movieapp/SceneManager.java
@@ -3,6 +3,7 @@ package com.example.movieapp;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
@@ -16,8 +17,20 @@ public class SceneManager {
 
     public static void switchScene(String fxmlPath) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(SceneManager.class.getResource(fxmlPath)));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+        Scene scene = new Scene(root);
+
+        // Get the screen size
+        Screen primaryScreen = Screen.getPrimary();
+        double screenWidth = primaryScreen.getVisualBounds().getWidth();
+        double screenHeight = primaryScreen.getVisualBounds().getHeight();
+
+        // Set the maximum window size to the screen size
+        primaryStage.setMaxWidth(screenWidth);
+        primaryStage.setMaxHeight(screenHeight);
+
+        primaryStage.setScene(scene);
+        primaryStage.setMaximized(false);
         primaryStage.setMaximized(true);
+        primaryStage.show();
     }
 }
